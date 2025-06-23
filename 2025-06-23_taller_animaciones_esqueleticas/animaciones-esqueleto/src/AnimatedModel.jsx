33fe3382d516176ccf3c9ad6228353ee8047bf99
Sebastian Muñoz <103005386+jumunozle@users.noreply.github.com>
2025-06-23 15:38:55 -0500

    Fixed bugs

--- 2025-06-23_taller_animaciones_esqueleticas/animaciones-esqueleto/src/AnimatedModel.jsx
+++ 2025-06-23_taller_animaciones_esqueleticas/animaciones-esqueleto/src/AnimatedModel.jsx
@@ -6,13 +6,21 @@ export function AnimatedModel({ animation = 'Idle', ...props }) {
   const model = useFBX('/personaje.fbx');
   const { actions, names } = useAnimations(model.animations, group);
 
-  useEffect(() => {
-    console.log('Animaciones disponibles:', names);
-    if (actions && actions[animation]) {
-      actions[animation].reset().fadeIn(0.2).play();
-      return () => actions[animation].fadeOut(0.2);
-    }
-  }, [animation]);
+useEffect(() => {
+  console.log('Animaciones disponibles:', names);
+
+  const action = actions?.[animation];
+  if (action) {
+    action.reset().fadeIn(0.2).play();
+
+    return () => {
+      if (action.isRunning()) {
+        action.fadeOut(0.2);
+      }
+    };
+  }
+}, [animation, actions]);
+
 
   return <primitive ref={group} object={model} scale={0.01} {...props} />;
 }
